8fced0dc9f17079e5efe05d8847725e38d8217db
KaKui <kakuitsang2010@gmail.com>
2021-12-15 21:44:49 +0800

    Initial Commit

---
+++ SliderTabBar/SliderTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderTabBarApp.swift
+//  SliderTabBar
+//
+//  Created by KaKui on 15/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct SliderTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
